4e72932cfe37ef0fd842a6643d4148b52282220e
farzan.zand <farzan.zand@king.com>
2023-10-24 14:16:16 +0200

    Created intro and went through basic steps.

---
+++ CIZan/FantasyCombatSimulator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    internal class FantasyCombatSimulator
+    {
+    static int strForPlayerOne = 30;
+    static int strForPlayerTwo = 45;
+    static string nameForPlayerOne = "Cizan";
+    static string nameForPlayerTwo = "Zanci";
+
+    static float damage; 
+
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Hello world!");
+        SharePlayerDamageValue(strForPlayerOne, nameForPlayerOne);
+        damage = BuffByMultiplyingByTwo(strForPlayerOne);
+        Console.WriteLine("Your buffed damage is " +  damage);
+
+        if (damage < 50)
+        {
+            Console.WriteLine("You lose!");
+        }
+        else
+        {
+            Console.WriteLine("You win!");
+        }
+    }
+
+    public static void SharePlayerDamageValue(int value, string playerName)
+    {
+        Console.WriteLine("Hi " + playerName);
+        Console.WriteLine("Your current strength is " + value);
+
+        damage = value * 1.5f; 
+
+        Console.WriteLine("Your damage is " +  damage);
+    }
+
+    public static float BuffByMultiplyingByTwo(float value)
+    {
+        float multipliedValue;
+        multipliedValue = value * 2;
+
+        return multipliedValue;
+    }
+
+
+
+
+}
