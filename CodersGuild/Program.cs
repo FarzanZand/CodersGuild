cef0fa13db308e4507173c8e9c510e98e6e5c38d
farzan.zand <farzan.zand@king.com>
2023-10-24 10:23:32 +0200

    Created demo project in c#

---
+++ CodersGuild/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Program
+{
+
+    public static void DemoMethod()
+    {
+        Console.WriteLine("This is a method in the same class.");
+    }
+
+    static void Main(string[] args)
+    {
+        TestHelpers testObject = new TestHelpers();
+
+        Console.WriteLine("Hello world! What is your name?");
+        String name = Console.ReadLine();
+        Console.WriteLine("Hello " + name);
+
+        
+        testObject.MethodExample(); 
+    }
+}
---
+++ CodersGuild/TestHelpers.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+internal class TestHelpers
+{
+    int age = 5; 
+    public void MethodExample()
+    {
+        Console.WriteLine("This is called from the class Program after we create an object!");
+    }
+}
